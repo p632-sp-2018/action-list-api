828ff82a38746214eac09bf9b752db81fc4cd291
varun-mach <vmaching@iu.edu>
2018-02-09 18:05:55 -0500

    Closes #1; Project Skeleton added

---
+++ src/main/java/com/actionlistapi/ActionListApiApplication.java
@@ -0,0 +1,12 @@
+package com.actionlistapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActionListApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActionListApiApplication.class, args);
+	}
+}

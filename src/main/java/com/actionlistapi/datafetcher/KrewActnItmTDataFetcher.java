1c77c2d556eb9b48c56c59a51e954aa5cbfbad29
Sagar Suresh Panchal <sagar.panchal11@gmail.com>
2018-03-18 23:12:19 -0400

    Updated the structure #Closes 43

--- src/main/java/com/actionlistapi/datafetcher/KrewActnItmTDataFetcher.java
+++ src/main/java/com/actionlistapi/datafetcher/KrewActnItmTDataFetcher.java
@@ -28,16 +28,20 @@ public class KrewActnItmTDataFetcher implements DataFetcher<KrewActnItmT> {
 		KrewActnItmT k = krewItmActnListRepository.findOne((String)arguments.get("id"));
 		k.setRequestLabel(ActionListUtil.getRequestCodeLabel(k.getRequestCode()));
 		k.setRouteLogUrl(k.getDocumentUrl()+ActionListConstants.ROUTE_LOG_URL);
+		
+		// Implemented to set the groupUrl in the KrimGrpT / group 
 		k.getGroup().setGroupUrl(ActionListConstants.GROUP_URL + k.getGroup().getId());
-		// Implemented to set the Person object
-		KrimEntityNmT p = k.getPrincipal().getPerson();
-		p.setDefaultName(p.getLastName() + ", " + p.getFirstName() + " " + p.getMiddleName());
-		p.setPersonUrl(ActionListConstants.PERSON_URL + k.getPrincipal().getPerson().getId());
-		if (p.getDefaultIndicator().equalsIgnoreCase("N") || p.getActiveIndicator().equalsIgnoreCase("N")) {
-			k.getPrincipal().setPerson(null);
+
+		// Implemented to set the personUrl inside KrimPrncplT / initiator
+		k.getInitiator().setPersonUrl(ActionListConstants.PERSON_URL + k.getInitiator().getUniversityId());
+		
+		// Implemented to set the entity details in KrimPrncplT pojo
+		KrimEntityNmT e = k.getInitiator().getEntity();
+		if (e.getDefaultIndicator().equalsIgnoreCase("N") || e.getActiveIndicator().equalsIgnoreCase("N")) {
+			k.getInitiator().setDefaultDisplayName(null);
 		}
 		else {
-			k.getPrincipal().setPerson(p);
+			k.getInitiator().setDefaultDisplayName(e.getLastName() + ", " + e.getFirstName() + " " + e.getMiddleName());
 		}
 		return k;
 	}

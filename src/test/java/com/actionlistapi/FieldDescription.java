d2966085acfe669722044b75adbdf7bdf0c9ba29
Shantanu Kotambkar <kotambkarshantanu@gmail.com>
2018-03-19 22:48:00 -0400

    Added Documentation for fields using Scalar Date

--- src/test/java/com/actionlistapi/FieldDescription.java
+++ src/test/java/com/actionlistapi/FieldDescription.java
@@ -5,6 +5,8 @@ import org.springframework.restdocs.payload.FieldDescriptor;
 import org.springframework.restdocs.payload.PayloadDocumentation;
 import org.springframework.restdocs.payload.RequestFieldsSnippet;
 
+import graphql.schema.GraphQLScalarType;
+
 
 public class FieldDescription {
 
@@ -18,54 +20,17 @@ public class FieldDescription {
 //    	  
 //	    	return new FieldDescriptor[]{
 //	    			PayloadDocumentation.fieldWithPath("[0]").description("The Krew Action Item Object")
-//	    			.andWithPrefix("[0]",FieldDescription.KrewActionItemIntegrationTestFields())
+//	    			.andWithPrefix("[0]",FieldDescription.FindAllActionItemIntegrationTestFields())
 //	    			
 //	    		};
 //    }
 
-//	PayloadDocumentation.fieldWithPath("[0].id").description("The Krew Action Item Identifier id"),
-//	PayloadDocumentation.fieldWithPath("[0].documentTypeLabel").description("DocumentTypeLabel for Krew Action Item")
-//    public static FieldDescriptor[] findOneIntegrationTestFields() {
-//    	  
-//		return new FieldDescriptor[]{
-//				PayloadDocumentation.fieldWithPath("documentTypeLabel").description("The Krew Action Item Document type Label"),
-//		
-//			};
-//    }
-//    
-//    public static FieldDescriptor[] PageResultsIntegrationTestFields() {
-//  	  
-//		return new FieldDescriptor[]{
-//					   PayloadDocumentation.fieldWithPath("[0]").description("The Krew Action Item Object for paging"),
-//        				   PayloadDocumentation.fieldWithPath("[0].documentTypeLabel").description("The Krew Action Item Document type Label"),
-//        				   PayloadDocumentation.fieldWithPath("[0].id").description("The Krew Action Item Identifier"),		   
-//				};
-//    }
-//    
-    
-    
-//    PayloadDocumentation.fieldWithPath("[0].document.id").description("The Krew Action Item Document Identifier"),
-//	   PayloadDocumentation.fieldWithPath("[0].document.routeStatus").description("The Krew Action Item Document Route Status"),
-//	   PayloadDocumentation.fieldWithPath("[0].document.initiator.id").description("The Krew Action Item Document Initiator Identifier"),
-//	   PayloadDocumentation.fieldWithPath("[0].document.initiator.username").description("The Krew Action Item Document Initiator username")
-//
     
 //	PayloadDocumentation.fieldWithPath("findKrewActionItem.id").description("The Krew Action Item Document type Label")
 //        				PayloadDocumentation.fieldWithPath("findKrewActionItem.creationDate").type(GraphQLScalarType.class).description("The Krew Action Item Creation Date")
 //                  		PayloadDocumentation.fieldWithPath("findKrewActionItem.id").description("The Krew Action Item Identifier")
     
-//	    			PayloadDocumentation.fieldWithPath("[0].group").description("Group object"),
-//	    			PayloadDocumentation.fieldWithPath("[0].group.id").description(" Group identifier"),
-//	    			PayloadDocumentation.fieldWithPath("[0].group.name").description("Group name"),
-//	    			PayloadDocumentation.fieldWithPath("[0].group.description").description("Group Description"),
-        				   
-        				  
-//        			PayloadDocumentation.fieldWithPath("findKrewActionItem.creationDate").type(GraphQLScalarType.class).description("The Krew Action Item Creation Date")
-//                      PayloadDocumentation.fieldWithPath("findKrewActionItem.id").description("The Krew Action Item Identifier")
-                     
-//        };
-//   }
-//	
+
 	
 //    public static FieldDescriptor[] FindAllActionItemIntegrationTestFields() {
 //  	  
@@ -95,6 +60,7 @@ public class FieldDescription {
         				   PayloadDocumentation.fieldWithPath("[0].requestCode").description("The Krew Action Item Request Code"),
         				   PayloadDocumentation.fieldWithPath("[0].requestLabel").description("The Krew Action Item Request Label"),
         				   PayloadDocumentation.fieldWithPath("[0].routeLogUrl").description("The Krew Action Item Route Log URL"), 
+        				   PayloadDocumentation.fieldWithPath("[0].creationDate").type(GraphQLScalarType.class).description("The Krew Action Item Creation Date")
 		};
     }
     
@@ -104,6 +70,7 @@ public class FieldDescription {
 
         				   PayloadDocumentation.fieldWithPath("[0].document.id").description("Document Identifier retrieved from KrewDocHdrT"),
         				   PayloadDocumentation.fieldWithPath("[0].document.routeStatus").description("Document Route Status retrieved from KrewDocHdrT"),
+        				   PayloadDocumentation.fieldWithPath("[0].document.lastApprovedDate").type(GraphQLScalarType.class).description("Last Approved Date for Document retrieved from KrewDocHdrT")
 
     
 		};
@@ -117,7 +84,8 @@ public class FieldDescription {
         				   PayloadDocumentation.fieldWithPath("[0].initiator.networkId").description("Network Identifier retrieved from KrimPrncplT"),
         				   PayloadDocumentation.fieldWithPath("[0].initiator.defaultDisplayName").description("Default Display Name retrieved from KrimPrncplT"),
         				   PayloadDocumentation.fieldWithPath("[0].initiator.personUrl").description("Person URL retrieved from KrimPrncplT"),
-        				   PayloadDocumentation.fieldWithPath("[0].initiator.active").description("Active Status retrieved from KrimPrncplT")
+        				   PayloadDocumentation.fieldWithPath("[0].initiator.active").description("Active Status retrieved from KrimPrncplT"),
+        				   PayloadDocumentation.fieldWithPath("[0].initiator.lastUpdateDate").type(GraphQLScalarType.class).description("Last Updated Date for Document retrieved from KrewDocHdrT")
     
 		};
     }
@@ -131,7 +99,8 @@ public class FieldDescription {
         				   PayloadDocumentation.fieldWithPath("[0].group.nameSpace").description("Group Name Space retrieved from KrimGrpT"),
         				   PayloadDocumentation.fieldWithPath("[0].group.description").description("Group description retrieved from KrimGrpT"),
         				   PayloadDocumentation.fieldWithPath("[0].group.groupUrl").description("Group URL retrieved from KrimGrpT"),
-        				   PayloadDocumentation.fieldWithPath("[0].group.active").description("Group Active Status retrieved from KrimGrpT")
+        				   PayloadDocumentation.fieldWithPath("[0].group.active").description("Group Active Status retrieved from KrimGrpT"),
+        				   PayloadDocumentation.fieldWithPath("[0].group.lastUpdateDate").type(GraphQLScalarType.class).description("Last Updated Date for Document retrieved from KrewDocHdrT")
     
 		};
     }
